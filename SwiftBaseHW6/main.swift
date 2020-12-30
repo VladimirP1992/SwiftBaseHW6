29fc7baa8f17fa72eb0ead90f983842265ef7643
Vladimir <p.vladimir.m@yandex.ru>
2020-12-30 23:58:56 +0300

    Task1 completed

--- SwiftBaseHW6/main.swift
+++ SwiftBaseHW6/main.swift
@@ -7,5 +7,42 @@
 
 import Foundation
 
-print("Hello, World!")
+//Task1: Queue
+class Queue <Type> {
+    private var elements: [Type] = []
+    
+    func count() -> Int {
+        return elements.count
+    }
+    
+    func push(_ element: Type) {
+        elements.append(element)
+    }
+    
+    func pop() -> Type? {
+        if count() > 0 {
+            return elements.removeFirst()
+        } else {
+            print("Queue is empty!")
+            return nil
+        }
+    }
+}
+
+var stringQueue: Queue<String> = Queue<String>()
+stringQueue.push("one")
+stringQueue.push("two")
+for _ in (0 ... stringQueue.count()) {
+    print(stringQueue.pop() ?? "")
+}
+
+var intQueue: Queue<Int> = Queue<Int>()
+intQueue.push(1)
+intQueue.push(2)
+for _ in (0 ... intQueue.count()) {
+    print(intQueue.pop() ?? "")
+}
+
+
+
 
